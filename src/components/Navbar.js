5f7dcd7151704d7bf0426f26a8fde268b11666bb
Gianluca Niccoli Mas <gianlumas4@gmail.com>
2023-04-18 21:48:13 -0300

    incorporacion de estilos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import CartWidget from './CartWidget';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Contacto</a></li>
-        <li><CartWidget /></li>
-      </ul>
+    <div className="nav-container">
+ <nav className="navigation">
+      <a href="/" className="brand-name">
+        Bookshop
+      </a>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">Books</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <CartWidget />
+          </li>
+        </ul>
+      </div>
     </nav>
-  );
-};
+    
+    </div>
+  )
+}
 
 export default Navbar;
