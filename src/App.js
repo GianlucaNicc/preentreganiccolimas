dea23530813dd82b148e4c5f6236865d84e13137
Gianluca Niccoli Mas <gianlumas4@gmail.com>
2023-04-18 21:50:45 -0300

    incorporacion de estilos

--- src/App.js
+++ src/App.js
@@ -11,24 +11,4 @@ const App = () => {
   );
 };
 
-
-const apiUrl = 'https://api.mercadopago.com/';
-
-const getProducts = async () => {
-  // Código para obtener los productos de la API de Mercado Pago
-}
-
-const createOrder = async (orderData) => {
-  // Código para crear una orden de compra en la API de Mercado Pago
-}
-
-const getPaymentMethods = async () => {
-  // Código para obtener los métodos de pago disponibles en la API de Mercado Pago
-}
-
-const executePayment = async (paymentData) => {
-  // Código para ejecutar el pago en la API de Mercado Pago
-}
-
-
 export default App;
